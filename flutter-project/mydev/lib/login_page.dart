fa62c560813e766bda56bedcc65f3da5f598ce2d
oguzhaneryilmaz <56223070+oguzhaneryilmaz@users.noreply.github.com>
2022-08-25 16:03:02 +0300

    register + tasarım
    register page yapıldır +
    login ve register page de bazı tasarım değişiklikleri yapıldı

--- flutter-project/mydev/lib/login_page.dart
+++ flutter-project/mydev/lib/login_page.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mydev/main.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,110 +15,134 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            //ana ekran ikonu
-
-            Icon(Icons.headset, size: 100),
-
-            //hello again
-            SizedBox(
-              height: 25,
-            ),
-            Text(
-              'Hello Again!',
-              style: GoogleFonts.bebasNeue(
-                fontSize: 60,
+      backgroundColor: Color(0xFFF1EEDB),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              //ana ekran ikonu
+              SizedBox(
+                height: 125,
               ),
-            ),
+              Icon(Icons.headset, size: 100),
 
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              'Welcome back, you\'ve been missed!',
-              style: TextStyle(
-                fontSize: 18,
+              //hello again
+              SizedBox(
+                height: 25,
               ),
-            ),
+              Text(
+                'Hello Again!',
+                style: GoogleFonts.bebasNeue(
+                  fontSize: 60,
+                ),
+              ),
 
-            SizedBox(
-              height: 20,
-            ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Welcome back, you\'ve been missed!',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
 
-            //email textfield
+              SizedBox(
+                height: 20,
+              ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: 'Email'),
+              //email textfield
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFD9B4A3),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: 'Email'),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            //password textfield
+              SizedBox(
+                height: 10,
+              ),
+              //password textfield
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(color: Colors.white),
-                    borderRadius: BorderRadius.circular(20)),
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20.0),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none, hintText: 'Password'),
-                    obscureText: true,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color(0xFFD9B4A3),
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none, hintText: 'Password'),
+                      obscureText: true,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
 
-            //sign in button
+              TextButton(
+                child: Text(
+                  'Forgot password?',
+                  style: TextStyle(
+                      color: Color(0xFF8D7688), fontWeight: FontWeight.bold),
+                ),
+                onPressed: () => Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Homepage())),
+              ),
 
-            Column(
-              children: [
-                Center(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(),
-                    child: Text('Sign In'),
-                    onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => Homepage())),
+              //sign in button
+
+              Column(
+                children: [
+                  Center(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF8D7688),
+                          textStyle: const TextStyle(fontSize: 15)),
+                      child: Text('Sign In'),
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => Homepage())),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Don\'t you have an account?',
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                Center(
-                  child: TextButton(
-                    child: Text('Register now!'),
-                    onPressed: () => Navigator.of(context).push(
-                        MaterialPageRoute(
-                            builder: (context) => RegisterPage())),
-                  ),
-                )
-              ],
-            )
-          ]),
+                  Center(
+                    child: TextButton(
+                      child: Text(
+                        'Register now!',
+                        style: TextStyle(
+                            color: Color(0xFF8D7688),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage())),
+                    ),
+                  )
+                ],
+              )
+            ]),
+          ),
         ),
       ),
     );
@@ -130,6 +155,7 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF1EEDB),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -155,21 +181,158 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'bura kayıt olma sayfası',
-              style: TextStyle(fontSize: 50),
+      backgroundColor: Color(0xFFF1EEDB),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 125,
+                ),
+
+                //register icon field
+
+                Icon(
+                  Icons.app_registration_rounded,
+                  size: 100,
+                ),
+                //registration page post
+
+                Text(
+                  'Registration Page',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 36,
+                  ),
+                ),
+
+                //register nickname field
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFD9B4A3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Nickname'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                //register password field
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFD9B4A3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Password'),
+                        obscureText: true,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFD9B4A3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: 'Password Again'),
+                        obscureText: true,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+
+                //register email field
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFFD9B4A3),
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Email'),
+                      ),
+                    ),
+                  ),
+                ),
+                //register button
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: ElevatedButton(
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage())),
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xFF8D7688),
+                          textStyle: const TextStyle(fontSize: 15))),
+                ),
+                // turn back to login page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Turn back to',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    TextButton(
+                      child: Text(
+                        'Login Page',
+                        style: TextStyle(
+                            color: Color(0xFF8D7688),
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => LoginPage())),
+                    ),
+                  ],
+                )
+              ],
             ),
-            ElevatedButton(
-                onPressed: () => Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => LoginPage())),
-                child: Text('Login page i aç'))
-          ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+// müzikler ve müzisyenler eklenecek
+// search bar yapılacak
+// veritabanı bağlantısı? yapılacak
