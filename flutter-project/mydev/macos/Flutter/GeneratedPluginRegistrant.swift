71630be6415e7fd05bbacfb940b6cfe4c38644e9
oguzhaneryilmaz <56223070+oguzhaneryilmaz@users.noreply.github.com>
2022-09-02 16:25:49 +0300

    çalışma + kişisel sayfa
    + kişisel sayfa boş bi şekilde eklendi
    + müzik çalma uygulaması üzerine çalışmaya devam ..

--- flutter-project/mydev/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter-project/mydev/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,16 +7,14 @@ import Foundation
 
 import audio_service
 import audio_session
-import audioplayers
-import just_audio
+import audioplayers_darwin
 import path_provider_macos
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
-  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
